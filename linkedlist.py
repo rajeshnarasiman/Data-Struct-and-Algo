669925e5554df753a1f86001160bccce677fd3ee
rajeshnarasiman <rajesh.narasiman@gmail.com>
2019-09-10 17:31:13 -0700

    Node creation, adding an element to front, middle and last

--- linkedlist.py
+++ linkedlist.py
@@ -15,6 +15,36 @@ class linkedlist:
         while temp:
             print(temp.data)
             temp = temp.next
+    #inserting a node in front of a node:
+    def push(self,new_data):
+        new_node = Node(new_data)
+
+        if self.head = None:
+            self.head = new_node
+            return
+        new_node.next = self.head
+        self.head = new_node
+
+    def update(prev_node,new_data):
+        new_node = Node(new_data)
+
+        if not prev_node:
+            self.head = new_node
+            return
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def append(self,new_data):
+        new_node = Node(new_data)
+
+        if self.head = None:
+            self.head = new_node
+            return
+
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
 
 
 #driver function
